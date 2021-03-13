541543a78888d5e8a00982659ee25c8f346c9f38
menezessn <mar_chelo10@hotmail.com>
2021-03-13 12:04:50 -0300

    Edições no script

--- Hora.js
+++ Hora.js
@@ -3,22 +3,29 @@ var msg = document.getElementById('msg');
 var img  = document.getElementById('foto');
 var data = new Date();
 var hora = data.getHours();
+var imagem = document.getElementById('imagem');
 msg.innerHTML=`<p>Agora são ${hora} horas</p>` ;
 
 if(hora<=1){
+    document.body.style.background = ('#977bb4');
     msg.innerHTML=`<p>Agora é ${hora} hora</p>` ;
-    msg.innerHTML += '<p><strong>Madrugada</strong></p>'
-    img.innerHTML= '<img id="imagem" src="imagens/noite.png" alt="foto do dia"></img>'
+    msg.innerHTML += '<p><strong>Boa madrugada!</strong></p>'
+    imagem.src= "imagens/noite.png";
 } else if (hora<5){
-    msg.innerHTML += '<p><strong>Madrugada</strong></p>'
+    document.body.style.background = ('#977bb4');
+    msg.innerHTML += '<p><strong>Boa madrugada!</strong></p>'
     img.innerHTML= '<img id="imagem" src="imagens/noite.png" alt="foto do dia"></img>'
 } else if(hora<12){
-    msg.innerHTML += '<p><strong>Manhã</strong></p>'
-    img.innerHTML= '<img id="imagem" src="imagens/manhã.png" alt="foto do dia"></img>'
+    document.body.style.background = ('#697279');
+    msg.innerHTML += '<p><strong>Bom dia!<!/strong></p>'
+    //img.innerHTML= '<img id="imagem" src="imagens/manhã.png" alt="foto do dia"></img>'
+    imagem.src= "imagens/manhã.png";
 }else if(hora<18){
-    msg.innerHTML += '<p><strong>Tarde</strong></p>'
+    document.body.style.background = ('#8d9db5');
+    msg.innerHTML += '<p><strong>Boa tarde!</strong></p>'
     img.innerHTML= '<img id="imagem" src="imagens/tarde.png" alt="foto do dia"></img>'
 } else {
-    msg.innerHTML += '<p><strong>Noite</strong></p>'
+    document.body.style.background = ('#977bb4');
+    msg.innerHTML += '<p><strong>Boa noite!</strong></p>'
     img.innerHTML= '<img id="imagem" src="imagens/noite.png" alt="foto do dia"></img>'
 }}
